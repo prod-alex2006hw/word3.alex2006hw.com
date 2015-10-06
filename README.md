fb17a1ed096679a14f1dbad41018650001c8efcc
Your Name <you@example.com>
2015-10-06 16:20:28 -0700

    working-with-fixtures

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # word3.alex2006hw.com
+  - runme : install meteor dependencies
+
+## notes
+  - words without userId will be publicly displayed
+  - logged in users will only see own's words
---
+++ word3.js
@@ -0,0 +1,75 @@
+
+if (Meteor.isClient) {
+  Meteor.subscribe('todos');
+  Meteor.subscribe('words');
+
+  Template.main.helpers({
+    todos: function(){
+          return Todos.find({}, {sort: {createdAt: -1}});
+        }
+  });
+
+  Template.main.events({
+    "submit .new-todo": function(event){
+      var text = event.target.text.value;
+      Meteor.call('addTodo', text);
+
+      // clear form
+      event.target.text.value = '';
+      // prevent default
+      return false;
+    },
+    "click .toggle-checked": function(){
+      Meteor.call('setChecked', this._id, !this.checked);
+    },
+    "click .delete-todo": function(){
+      if(confirm('Are you sure?')){
+        Meteor.call('deleteTodo', this._id);
+      }
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+  });
+}
+
+if(Meteor.isServer) {
+  Meteor.publish('todos', function(){
+    return Todos.find({userId: this.userId});
+  });
+  Meteor.publish('words', function(){
+    return Words.find();
+  });
+}
+
+Meteor.methods({
+  addTodo: function(text){
+    if(!Meteor.userId()) {
+      throw new Meteor.Error('Not Authorized');
+    }
+    Todos.insert({
+      text: text,
+      createdAt: new Date(),
+      userId: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTodo: function(todoId){
+    var todo = Todos.findOne(todoId);
+    if(todo.userId !== Meteor.userId()){
+      throw new Meteor.Error('Not Authorized');
+    }
+    Todos.remove(todoId);
+  },
+  setChecked: function(todoId, setChecked){
+    var todo = Todos.findOne(todoId);
+    if(todo.userId !== Meteor.userId()){
+        throw new Meteor.Error('Not Authorized');
+      }
+    Todos.update(todoId, {$set: {checked: setChecked}});
+    }
+
+});
+
+
